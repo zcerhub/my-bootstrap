179817c2bd7ee2ee80834a532811e74ba1090d5e
gateszhang <gates@zhang.com>
2023-03-09 20:59:47 +0800

    test

--- logging-demo/src/main/java/com/example/loggingdemo/Main.java
+++ logging-demo/src/main/java/com/example/loggingdemo/Main.java
@@ -1,8 +1,8 @@
 package com.example.loggingdemo;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
 
@@ -17,8 +17,8 @@ public class Main {
 	}*/
 
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(Main.class);
-		logger.info("hello, world!");
+		Logger logger = LogManager.getLogger(Main.class);
+		logger.error("hello, world!");
 	}
 
 }
--- logging-demo/src/test/java/com/example/loggingdemo/LoggingDemoApplicationTests.java
+++ logging-demo/src/test/java/com/example/loggingdemo/LoggingDemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.loggingdemo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LoggingDemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.loggingdemo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LoggingDemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
