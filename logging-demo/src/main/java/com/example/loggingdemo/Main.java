a31d5a2c33351192cf2e14b9a75b3e4526dce94b
gateszhang <gates@zhang.com>
2023-03-09 20:49:44 +0800

    test

--- logging-demo/src/main/java/com/example/loggingdemo/Main.java
+++ logging-demo/src/main/java/com/example/loggingdemo/Main.java
@@ -1,13 +1,21 @@
 package com.example.loggingdemo;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.logging.Logger;
 
 public class Main {
 
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Main.class.getName());
 		logger.info("hello, world!");
+	}*/
+
+	public static void main(String[] args) {
+		Log log = LogFactory.getLog(Main.class);
+		log.info("hello, world!");
 	}
 
 }
