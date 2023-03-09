859b05696eb09e48126289d911883b3856377c4c
gateszhang <gates@zhang.com>
2023-03-09 20:47:20 +0800

    test

---
+++ logging-demo/src/main/java/com/example/loggingdemo/Main.java
@@ -0,0 +1,13 @@
+package com.example.loggingdemo;
+
+
+import java.util.logging.Logger;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Main.class.getName());
+		logger.info("hello, world!");
+	}
+
+}
